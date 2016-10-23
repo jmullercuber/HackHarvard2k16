6ebc713d4077a99cee1d493362462c768e7a22c2
Joey <jmullercuber@gmail.com>
2016-10-22 23:20:46 -0400

    Deleted old main.py, replaced with main_2.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import cognitive, json, os.path
+import cognitive, json, os.path, operator
 # Establish connections to internet sources
 from RedditScraper import RedditScraper
 
 
 if __name__ == "__main__":
 	# collect text data from them
-	reddit = RedditScraper()
-	internet_documents = {  'reddit-politics': reddit.get_documents("politics", 3)}
-			
+	'''reddit = RedditScraper()
+	internet_documents = {  'reddit-politics': reddit.get_documents("politics", 3),
+							'reddit-the-donald': reddit.get_documents("the_donald", 3)  }'''
+
 	# For every source analyze text data with the Microsoft Cognitive Services Text Analytics API
 	s_file_path = "./sentiments.data"
 	if os.path.isfile(s_file_path):
@@ -33,18 +34,44 @@ if __name__ == "__main__":
 			source: cognitive.documents_to_topics(docs)
 			for source, docs in internet_documents.items()
 		}
-		t_file = open('sentiments.data', 'w')
+		t_file = open('topics.data', 'w')
 		t_file.write(json.dumps(topics))
 		t_file.close()
+	
+	
+	
+	c = {}
+	
+	for source in topics:
+		c[source] = []
+		topic_objs = topics[source]['result']['operationProcessingResult']['topics']
+		topic_asgn = topics[source]['result']['operationProcessingResult']['topicAssignments']
+		source_sents = sentiments[source]['result']['documents']
+		for topic in topic_objs:
+			t_id = topic['id']
+			t_key = topic['keyPhrase']
+			topic_sents = []
+			for j in topic_asgn:
+				if j['topicId'] == t_id:
+					for k in source_sents:
+						if k['id'] == j['documentId']:
+							topic_sents.append((k['score'], j['distance']))
+			if len(topic_sents) >= 4:
+				c[source].append({t_key: topic_sents})
+	
 	
 	# Do something to turn topics, and sentiments to desired format c
 	# c = f(topics, sentiments)
 	
 	#### At this point we have data of the form: {'internet_source1': [{'topic1': [sentiment_array1], 'topic2': [sentiment_array2] ...}]}
-	### Example: {'reddit': [{'presidential election': [0.232, 0.978, 0.315], 'puppies': [1.000, 0.999, 0.978] ... }]}
+	### Example: {'reddit': [{'presidential election': [(0.232, .5), (0.978, .25), (0.315, .01)], 'puppies': [(1.000, .2), (0.999, .7), (0.978, 1)] ... }]}
 	
 	# Data open for reduction
 	
 	# Visualize data
+
+	from plot import *
+	bg = bar_graph(c['reddit-politics'])
+	g = bar_graph(c['reddit-the-donald'])
 
 # Cool, we're done!
