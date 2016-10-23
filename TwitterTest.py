b6e6cdf53ab9ff4ecabf9a2f9728e055c811ee66
Simon Sun <ssun@college.harvard.edu>
2016-10-22 20:17:11 -0400

    trending twitter topics added

--- TwitterTest.py
+++ TwitterTest.py
@@ -4,23 +4,40 @@ key = 'kFxjNI3AlMiWGCVVIdg2mwLVf'
 secret = 'VbvyqLW2B0LB3BNCVZfJaSD7djvAbYuQgBvWnoz7oycoINqKoF'
 
 def getBearer(key, secret):
-	#Ideally should url encode the key and secret,
-	#but it will work ok without for now
+    #Ideally should url encode the key and secret,
+    #but it will work ok without for now
 
-	credentials = base64.b64encode(key + ':' + secret)
-	headers = {'Authorization': 'Basic ' + credentials, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
-	conn = httplib.HTTPSConnection('api.twitter.com')
-	conn.request('POST', '/oauth2/token', 'grant_type=client_credentials', headers)
-	response = conn.getresponse()
-	bearer = ast.literal_eval(response.read())['access_token']
-	return bearer
+    credentials = base64.b64encode(key + ':' + secret)
+    headers = {'Authorization': 'Basic ' + credentials, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+    conn = httplib.HTTPSConnection('api.twitter.com')
+    conn.request('POST', '/oauth2/token', 'grant_type=client_credentials', headers)
+    response = conn.getresponse()
+    bearer = ast.literal_eval(response.read())['access_token']
+    return bearer
 
+#Gets the most recent tweets for a hashtag
 def getResults(bearer, hashtag):
-	url = '/1.1/search/tweets.json?q=%23' + hashtag
-	headers = {'Authorization': 'Bearer ' + bearer}
-	conn = httplib.HTTPSConnection('api.twitter.com')
-	conn.request('GET', url, '', headers)
-	response = conn.getresponse().read()
-	return response
+    url = '/1.1/search/tweets.json?q=%23' + hashtag
+    headers = {'Authorization': 'Bearer ' + bearer}
+    conn = httplib.HTTPSConnection('api.twitter.com')
+    conn.request('GET', url, '', headers)
+    response = conn.getresponse().read()
+    return response
 
-print getResults(getBearer(key, secret), 'hamilton')
+#Returns a list of the top 10 trending topics and their tweet counts
+def getTrending(bearer, woeid):
+    url = '/1.1/trends/place.json?id=' + str(woeid)
+    headers = {'Authorization': 'Bearer ' + bearer}
+    conn = httplib.HTTPSConnection('api.twitter.com')
+    conn.request('GET', url, '', headers)
+    response = conn.getresponse().read()
+    tweet_data = json.loads(response)
+    
+    trends = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] #Ideally this would not need to be explicitly done... Python is weird
+
+    for count in range(0, 10):
+        trends[count] = (tweet_data[0]['trends'][count]['name'], tweet_data[0]['trends'][count]['tweet_volume']) 
+
+    return trends
+
+print getTrending(getBearer(key, secret), 23424977) #test
